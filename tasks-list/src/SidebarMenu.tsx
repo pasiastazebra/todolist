4659c5a807913a02f36b462ef521543677665b3b
Konrad <vobis08gw@gmail.com>
2023-11-13 20:01:47 +0100

    polished styling

--- tasks-list/src/SidebarMenu.tsx
+++ tasks-list/src/SidebarMenu.tsx
@@ -1,25 +1,41 @@
 function SidebarMenu() {
   return (
     <div className="flex flex-1">
-      <aside className="flex-col bg-sky-700 text-gray-200 py-3 px-7  min-w-[200px] w-[200px] ">
+      <aside className="flex-col bg-sky-700 text-gray-200 py-3 px-7  min-w-[200px] w-[230px] ">
         <div className="flex flex-col items-left">
-          <h1 className="text-2xl font-bold">Menu</h1>
+          <h1 className="text-2xl font-bold">Tasks system</h1>
         </div>
         <div className="flex flex-col items-left mt-4">
-          <div className="inline-flex flex-col items-start bg-sky-600/50 rounded-lg p-3 my-2">
-            <button>Tasks</button>
+          <div className="inline-flex flex-col items-start bg-sky-600/50 rounded-lg p-3 my-2 w-[100%]">
+            <button className="hover:bg-sky-500/50 duration-150 w-[100%] text-left px-2 rounded-md">
+              Tasks
+            </button>
             <div className="inline-flex flex-col items-start ml-5">
-              <button>Planed</button>
-              <button>Ongoing</button>
-              <button>Completed</button>
+              <button className="hover:bg-sky-500/50 duration-150 w-[100%] text-left px-2 rounded-md">
+                Planed
+              </button>
+              <button className="hover:bg-sky-500/50 duration-150 w-[100%] text-left px-2 rounded-md">
+                Ongoing
+              </button>
+              <button className="hover:bg-sky-500/50 duration-150 w-[100%] text-left px-2 rounded-md">
+                Completed
+              </button>
             </div>
           </div>
-          <div className="inline-flex flex-col items-start bg-sky-600/50 rounded-lg p-3 my-2">
-            <button>Settings</button>
+          <div className="inline-flex flex-col items-start bg-sky-600/50 rounded-lg p-3 my-2 ">
+            <button className="hover:bg-sky-500/50 duration-150 w-[100%] text-left px-2 rounded-md">
+              Settings
+            </button>
             <div className="inline-flex flex-col items-start ml-5">
-              <button>Dark mode</button>
-              <button>Custom statuses</button>
-              <button>Clear all data</button>
+              <button className="hover:bg-sky-500/50 duration-150 w-[100%] text-left px-2 rounded-md">
+                Dark mode
+              </button>
+              <button className="hover:bg-sky-500/50 duration-150 w-[100%] text-left px-2 rounded-md">
+                Statuses
+              </button>
+              <button className="hover:bg-sky-500/50 duration-150 w-[100%] text-left px-2 rounded-md">
+                Clear all data
+              </button>
             </div>
           </div>
         </div>
