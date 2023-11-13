9920ba6cbb1e50e8f91f21f717e569dce3c27968
Konrad <vobis08gw@gmail.com>
2023-11-13 20:21:37 +0100

    moved bg color and added second menu

--- tasks-list/src/main.tsx
+++ tasks-list/src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import SidebarMenu from "./SidebarMenu.tsx";
+import TasksBar from "./TasksBar.tsx";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className="bg-gray-100 flex flex-col h-[100vh]">
+    <div className="flex flex-col flex-wrap content-start al h-[100vh]">
       <SidebarMenu />
+      <TasksBar />
     </div>
   </React.StrictMode>
 );
