c6e4f51f57b1afba5ce8325d1608796bc4606b15
Konrad <vobis08gw@gmail.com>
2023-11-13 19:49:14 +0100

    shading

--- tasks-list/src/SidebarMenu.tsx
+++ tasks-list/src/SidebarMenu.tsx
@@ -1,12 +1,12 @@
 function SidebarMenu() {
   return (
     <div className="flex flex-1">
-      <aside className="flex-col bg-indigo-600 text-white py-3 pl-7 min-w-[200px] w-[200px] ">
+      <aside className="flex-col bg-sky-700 text-gray-200 py-3 px-7  min-w-[200px] w-[200px] ">
         <div className="flex flex-col items-left">
           <h1 className="text-2xl font-bold">Menu</h1>
         </div>
         <div className="flex flex-col items-left mt-4">
-          <div className="inline-flex flex-col items-start">
+          <div className="inline-flex flex-col items-start bg-sky-600/50 rounded-lg p-3 my-2">
             <button>Tasks</button>
             <div className="inline-flex flex-col items-start ml-5">
               <button>Planed</button>
@@ -14,7 +14,7 @@ function SidebarMenu() {
               <button>Completed</button>
             </div>
           </div>
-          <div className="inline-flex flex-col items-start">
+          <div className="inline-flex flex-col items-start bg-sky-600/50 rounded-lg p-3 my-2">
             <button>Settings</button>
             <div className="inline-flex flex-col items-start ml-5">
               <button>Dark mode</button>
